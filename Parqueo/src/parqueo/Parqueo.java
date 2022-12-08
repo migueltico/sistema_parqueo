81aafb4f96fd7972afbc5cf03162c20b772e327b
Miguel Esquivel <miguel.gesquivel@gmail.com>
2022-12-07 23:22:37 -0600

    Se ajusta Objecto ParqueoEspacio y clase ParqueoController

--- Parqueo/src/parqueo/Parqueo.java
+++ Parqueo/src/parqueo/Parqueo.java
@@ -12,39 +12,51 @@ import javax.swing.JOptionPane;
  */
 public class Parqueo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    ParqueoController controller = new ParqueoController();
 
-        ParqueoController controller = new ParqueoController();
+    //Menu
+    boolean keepMenu = true;
+    String welcomeDialog =
+      "Bienvenido al sistema de parqueo de Qurayn Abu al Bawl \n\n";
+    do {
+      try {
+        int opcion = Integer.parseInt(
+          JOptionPane.showInputDialog(
+            null,
+            welcomeDialog +
+            "Seleccione una de las siguientes opciones.\n 1- Registrar Ingreso.\n 2- Mostrar Parqueo.\n 5- Salir",
+            "Menu",
+            JOptionPane.OK_OPTION
+          )
+        );
 
-        //Menu
-        boolean keepMenu = true;
-        String welcomeDialog = "Bienvenido al sistema de parqueo de Qurayn Abu al Bawl \n\n";
-        do {
-
-            int opcion = Integer.parseInt(JOptionPane.showInputDialog(welcomeDialog + "Seleccione una de las siguientes opciones.\n 1- Registrar Ingreso.\n 2- Mostrar Parqueo.\n 5- Salir"));
-
-            if (opcion > 0 && opcion <= 5) {
-
-                switch (opcion) {
-                    case 1 ->
-                        controller.RegistrarIngreso();
-                    case 2 ->
-                        controller.MostrarParqueo();
-                    case 5 ->
-                        keepMenu = false;
-                    default ->
-                        throw new AssertionError();
-                }
-
-            } else {
-                JOptionPane.showMessageDialog(null, "La opcion ingresada no coincide con las opciones mostradas");
-            }
-
-        } while (keepMenu);
-
-    }
-
+        if (opcion > 0 && opcion <= 5) {
+          switch (opcion) {
+            case 1 -> controller.RegistrarIngreso();
+            case 2 -> controller.MostrarParqueo();
+            case 5 -> keepMenu = false;
+            default -> throw new AssertionError();
+          }
+        } else {
+          JOptionPane.showMessageDialog(
+            null,
+            "La opcion ingresada no coincide con las opciones mostradas",
+            "Error",
+            JOptionPane.ERROR_MESSAGE
+          );
+        }
+      } catch (Exception e) {
+        JOptionPane.showMessageDialog(
+          null,
+          "La opcion ingresada no coincide con las opciones mostradas",
+          "Error",
+          JOptionPane.ERROR_MESSAGE
+        );
+      }
+    } while (keepMenu);
+  }
 }
