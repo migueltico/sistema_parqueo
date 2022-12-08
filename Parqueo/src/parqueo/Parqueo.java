16509a7e2091b37a879a4f7f9380c0f7ac931a1c
Miguel Esquivel <miguel.gesquivel@gmail.com>
2022-12-07 19:07:43 -0600

    First commit project

---
+++ Parqueo/src/parqueo/Parqueo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parqueo;
+
+/**
+ *
+ * @author migue
+ */
+public class Parqueo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
